d52a0c7f566afd16520855de37b7334985f217e6
uayeb85 <gustavo.aa18@gmail.com>
2021-07-23 13:15:44 -0500

    snipcart

--- src/.temp/now.js
+++ src/.temp/now.js
@@ -1,1 +1,1 @@
-export default 1627063244863
+export default 1627064103496
--- src/templates/Product.vue
+++ src/templates/Product.vue
@@ -29,7 +29,7 @@
                                   :data-item-name="$page.product.title"
                                   :data-item-price="$page.product.price"
                                   :data-item-image="$page.product.ecommerceImage"
-                                  :data-item-url="'uayeb-commerce.netlify.app' + $page.product.path"
+                                  :data-item-url="'https://uayeb-commerce.netlify.app/' + $page.product.path"
                                   :data-item-description="$page.product.about"
                                   data-item-has-taxes-included="true"
                                   data-item-custom1-name="Long message"
